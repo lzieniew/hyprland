646c0994e87ec0f9724befb5f56736aa1001f60d
lzieniew <lukzieniewicz@gmail.com>
2023-09-22 19:51:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ yay -S kitty waybar-hyprland \
     polkit-gnome python-requests starship \
     swappy grim slurp pamixer brightnessctl gvfs \
     bluez bluez-utils lxappearance xfce4-settings \
-    dracula-gtk-theme tela-icon-theme dracula-icons-git \ 
+    dracula-gtk-theme tela-icon-theme dracula-icons-git \
     xdg-desktop-portal-hyprland hyprpicker wlsunset \
     zsh zsh-theme-powerlevel10k konsave
 
