17e613ea0bf9dd02e93de561049f9d5e08c62617
Łukasz Zieniewicz <lukzieniewicz@gmail.com>
2024-04-16 20:49:58 +0200

    Improve script

--- themes/gruvbox_light_theme/set_gruvbox_light_theme.sh
+++ themes/gruvbox_light_theme/set_gruvbox_light_theme.sh
@@ -1,10 +1,10 @@
 script_dir=$(dirname "$(realpath $0)")
 theme_file="$script_dir/gruvbox_light.json"
 wal -l --theme $theme_file
+echo "light" >~/.config/nvim/nvimtheme
 
 echo "======================"
-echo "WARNING You have to do some things manually:"
+echo "WARNING You may have to do some things manually:"
 echo "======================"
 echo "Change wallpaper in ~/.config/hypr/hyprland.conf"
 echo "Change name of ~/.config/waybar/gruvbox_style.css to style.css"
-echo "Change background option in neovim options.lua to dark"
