9342358fcfe3fd98f39bfc6cfec0090bd2089af1
Łukasz Zieniewicz <lukzieniewicz@gmail.com>
2024-04-21 10:59:10 +0200

    LSD

--- install.sh
+++ install.sh
@@ -11,16 +11,16 @@ sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 cd ..
 
-
+sudo pacman -S lsd
 yay -S kitty waybar-hyprland rofi-lbonn-wayland-git \
-    swaybg swaylock-effects wlogout mako thunar \
-    ttf-jetbrains-mono-nerd noto-fonts-emoji \
-    polkit-gnome python-requests starship \
-    swappy grim slurp pamixer brightnessctl gvfs \
-    bluez bluez-utils lxappearance xfce4-settings \
-    dracula-gtk-theme tela-icon-theme dracula-icons-git \
-    xdg-desktop-portal-hyprland hyprpicker wlsunset \
-    zsh zsh-theme-powerlevel10k konsave hyprlshot --noconfirm
+	swaybg swaylock-effects wlogout mako thunar \
+	ttf-jetbrains-mono-nerd noto-fonts-emoji \
+	polkit-gnome python-requests starship \
+	swappy grim slurp pamixer brightnessctl gvfs \
+	bluez bluez-utils lxappearance xfce4-settings \
+	dracula-gtk-theme tela-icon-theme dracula-icons-git \
+	xdg-desktop-portal-hyprland hyprpicker wlsunset \
+	zsh zsh-theme-powerlevel10k konsave hyprlshot --noconfirm
 
 chsh -s /bin/zsh
 sudo chsh -s /bin/zsh
